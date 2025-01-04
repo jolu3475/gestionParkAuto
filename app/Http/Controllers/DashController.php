193e91a580d81d50d40acefe751f9df157c0528a
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2025-01-04 15:15:32 +0300

    Ajout de la gestion des utilisateurs, des voitures et des intitulés : création, modification et affichage dans le tableau de bord

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -13,24 +13,28 @@ use App\Http\Requests\addVoiture;
 class DashController extends Controller
 {
     //
+    /* Acceil de la dashboard */
     public function index (Request $request) {
         return view('Dashboard.index');
     }
 
+    /* Parametre du compte utilisateur */
     public function profile (Request $request) {
         return view('Dashboard.users.profile');
     }
 
+    /* Gestion des utilisateurs */
     public function users (Request $request) {
         $users = User::all();
         return view('Dashboard.users', compact('users'));
     }
 
+    /* Visualiser l'utilisateur */
     public function user (Request $request, User $user) {
-        // dd($user);
         return view('Dashboard.users.user', compact('user'));
     }
 
+    /* Ajouter en temps que su ou la retirer */
     public function addSu ( Request $request, User $user ) {
         if ($user->su == 1) {
             $users = User::where('su', 1)->count();
@@ -43,6 +47,7 @@ class DashController extends Controller
         return redirect()->route('dash.users.user', $user)->with('success', $user->su == 1 ? 'Utilisateur ajouter en tant que super utilisateur' : 'Utilisateur retirer du groupe des super utilisateurs');
     }
 
+    /* Supprimer l'utilisateur */
     public function deleteUser ( Request $request, User $user ) {
         if ($user->su == 1) {
             return redirect()->route('dash.users.user', $user)->with('error', 'Impossible de supprimer le super utilisateur');
@@ -51,6 +56,7 @@ class DashController extends Controller
         return redirect()->route('dash.users.users')->with('success', 'Utilisateur supprimer avec succès');
     }
 
+    /* Modifier l'utilisateur */
     public function updateUser ( Request $request, User $user ) {
         $request->validate([
             'username' => ['required', 'string', Rule::unique('users')->ignore($user->id)],
@@ -62,6 +68,7 @@ class DashController extends Controller
         return redirect()->route('dash.profile', $user)->with('success', 'Utilisateur modifier avec succès');
     }
 
+    /* Modifier le mot de passe */
     public function updatePass ( Request $request, user $user) {
         $request->validate([
             'password' => ['required', 'string', 'min:4', 'confirmed'],
@@ -71,25 +78,30 @@ class DashController extends Controller
         return redirect()->route('dash.profile', $user)->with('success', 'Mots de passe modifier avec succès');
     }
 
+    /* Visualiser les maintances */
     public function maintenance (Request $request) {
         $maintenances = Maintenance::all();
         return view('Dashboard.maintenance', compact('maintenances'));
     }
 
+    /* Visualiser les voitures */
     public function voiture (Request $request) {
         $voiture = Voiture::get();
         return view('Dashboard.voiture', compact('voiture'));
     }
 
+    /* Ajouter une voiture */
     public function ajoutVoiture (addVoiture $request){
         Voiture::create($request->validated());
         return redirect()->route('dash.voiture')->with('success', 'La voiture a été ajouter avec succes');
     }
 
+    /* Visualiser la voiture */
     public function voi (Request $request, voiture $voiture){
         return view('Dashboard.voiture.voiture', compact('voiture'));
     }
 
+    /* Modifier la voiture */
     public function editVoiture (Request $request, voiture $voiture) {
         $request->validate([
             'plaque' => ['required', Rule::unique('voitures')->ignore($voiture->id)],
@@ -103,11 +115,13 @@ class DashController extends Controller
         return redirect()->route('dash.voi', $voiture->id)->with('success', 'Voiture modifier avec succès');
     }
 
+    /* Visualiser les intituler */
     public function intituler (Request $request) {
         $intituler = Reparation::all();
         return view('Dashboard.intituler', compact('intituler'));
     }
 
+    /* Ajout d'une intituler */
     public function addInt (Request $request) {
         $request->validate([
             'type' => ['required', 'string', 'unique:reparations']
@@ -116,10 +130,12 @@ class DashController extends Controller
         return redirect()->route('dash.intituler')->with('success', 'Intituler ajouter avec succès');
     }
 
+    /* Visualiser l'intituler */
     public function editInt (Request $request, Reparation $reparation) {
         return view('Dashboard.intituler.edit', compact('reparation'));
     }
 
+    /* Modifier l'intituler */
     public function updateInt (Request $request, Reparation $reparation) {
         $request->validate([
             'type' => ['required', 'string', Rule::unique('reparations')->ignore($reparation->id)]
