2747a770ff1eb1bdc06c70f77d1e7f6fb38a869a
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2025-01-05 09:22:00 +0300

    Amélioration de la gestion des classes CSS dans la barre latérale : ajout de la logique pour mettre en surbrillance l'élément actif en fonction de la route actuelle

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -1,27 +1,40 @@
+@php
+    $route = Route::currentRouteName();
+@endphp
+
 <aside id="logo-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
             <li>
-                <a href="{{ route('dash.index') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a href="{{ route('dash.index') }}" {{-- class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" --}} @class([
+                    'flex items-center p-2 text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700 group',
+                    'hover:bg-gray-100' => !Str::startsWith($route, 'dash.index'),
+                    'bg-gray-100' => Str::startsWith($route, 'dash.index'),
+                ])>
                     <i
                         class="fa-solid fa-chart-pie flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"></i>
                     <span class="ms-3">Dashboard</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('dash.maintenance') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a href="{{ route('dash.maintenance') }}" {{-- class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" --}} @class([
+                    'flex items-center p-2 text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700 group',
+                    'hover:bg-gray-100' => !Str::startsWith($route, 'dash.main'),
+                    'bg-gray-100' => Str::startsWith($route, 'dash.main'),
+                ])>
                     <i
                         class="fa-solid fa-screwdriver-wrench flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Maintenance</span>
                 </a>
             </li>
             <li>
-                <a href="{{ route('dash.voiture') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a href="{{ route('dash.voiture') }}" {{-- class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" --}} @class([
+                    'flex items-center p-2 text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700 group',
+                    'hover:bg-gray-100' => !Str::startsWith($route, 'dash.voiture'),
+                    'bg-gray-100' => Str::startsWith($route, 'dash.voiture'),
+                ])>
                     <i
                         class="fa-solid fa-car flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Voiture</span>
--- resources/views/component/user.blade.php
+++ resources/views/component/user.blade.php
@@ -1,14 +1,20 @@
 <li>
-    <a href="{{ route('dash.users.users') }}"
-        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+    <a href="{{ route('dash.users.users') }}" {{-- class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" --}} @class([
+        'flex items-center p-2 text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700 group',
+        'hover:bg-gray-100' => !Str::startsWith($route, 'dash.user'),
+        'bg-gray-100' => Str::startsWith($route, 'dash.user'),
+    ])>
         <i
             class="fa-solid fa-users flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"></i>
         <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
     </a>
 </li>
 <li>
-    <a href="{{ route('dash.intituler') }}"
-        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+    <a href="{{ route('dash.intituler') }}" {{-- class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" --}} @class([
+        'flex items-center p-2 text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700 group',
+        'hover:bg-gray-100' => !Str::startsWith($route, 'dash.intituler'),
+        'bg-gray-100' => Str::startsWith($route, 'dash.intituler'),
+    ])>
         <i
             class="fa-solid fa-pen flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"></i>
         <span class="flex-1 ms-3 whitespace-nowrap">Intituler</span>
